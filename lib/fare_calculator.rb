550e827bd1a815f64f4c13c2e91f81331927085c
Dines Poulsen <dinespoulsen84@gmail.com>
2016-11-21 16:40:58 +0000

    Added a fare calculator class

--- lib/fare_calculator.rb
+++ lib/fare_calculator.rb
@@ -1,11 +1,21 @@
 class FareCalculator
 
-  attr_reader :price
+  attr_accessor :zones, :price
 
   MINIMUM_FARE = 1
+  ZONE_PRICE = 1
 
   def initialize(price = MINIMUM_FARE)
     @price = price
+    @zones
+  end
+
+  def zones_travelled(zone_entry, zone_exit)
+    self.zones = (zone_entry - zone_exit).abs
+  end
+
+  def calculate_price
+    self.price = zones * ZONE_PRICE
   end
 
 end
